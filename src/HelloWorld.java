cf2cbe1e3afebb96add1ef9ca0dcbbb4806ccf8d
Kate Keil <keilka@miamioh.edu>
2020-04-29 14:03:58 -0300

    My first commit in Git

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Practicing using git/git hub for Lab 13
+ * @author Kate Keil
+ * April 29, 2020
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+	}
+
+}
